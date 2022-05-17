07133002294197e11b859e54ea0c723be02a3fdc
Voltair <92871565+Voltairxx@users.noreply.github.com>
2022-05-17 15:04:09 +0200

    Add files via upload
    FR: création du deck + affichage
    EN: deck creation + display

--- Blacjack/src/Blackjack.java
+++ Blacjack/src/Blackjack.java
@@ -4,15 +4,8 @@ public class Blackjack {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Card c0 = new Card(0,1);
-		Card c1 = new Card(1,10);
-		Card c2 = new Card(2,11);
-		Card c3 = new Card(3,13);
-		
-		System.out.println(c0);
-		System.out.println(c1);
-		System.out.println(c2);
-		System.out.println(c3);
+		Deck d = new Deck();
+		System.out.println(d);
 	}
 
 }
--- Blacjack/src/Deck.java
+++ Blacjack/src/Deck.java
@@ -4,6 +4,24 @@ public class Deck {
 	
 	public Deck() {
 		this.content = new Card[52];
-		this.content[] = new Card();
+		
+		int nbCartes = 0;
+		for (int j = 0; j <= 3; j++) {
+			for (int i=1; i <= 13; i++) {
+				this.content[nbCartes]= new Card (j,i);
+				nbCartes++;
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		String result = "";
+		for(int i = 0; i < this.content.length; i++) {
+			Card c = this.content[i];
+			result += c.toString();
+			result += ",";
+		}
+		return result;
 	}
 }
