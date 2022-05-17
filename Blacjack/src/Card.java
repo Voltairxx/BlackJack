bb58b5630b7e4f3cf3bb42a8f18e02dcecfd4181
Voltair <92871565+Voltairxx@users.noreply.github.com>
2022-05-17 13:26:18 +0200

    Add files via upload

---
+++ Blacjack/src/Card.java
@@ -0,0 +1,18 @@
+
+public class Card {
+	public char color;
+	public int value;
+	
+	public Card (int color, int value) {
+		this.value = value;
+		if (color == 0) {this.color = 'H';}
+		if (color == 1) {this.color = 'D';}
+		if (color == 2) {this.color = 'S';}
+		if (color == 3) {this.color = 'C';}
+		
+	}
+	@Override
+	public String toString() {
+		return String.valueOf(this.color) + String.valueOf(this.value);
+	}
+}
---
+++ Blacjack/src/Deck.java
@@ -0,0 +1,9 @@
+
+public class Deck {
+	private Card[] content;
+	
+	public Deck() {
+		this.content = new Card[52];
+		this.content[] = new Card();
+	}
+}
